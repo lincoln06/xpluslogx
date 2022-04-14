f915625fbc62fd54a31f8ba033bd8e5dbcb78f66
MJL <maciekjl1993@gmail.com>
2022-04-14 19:47:50 +0200

    stack overflow, but close

--- xpluslogx/xpluslogx/Program.cs
+++ xpluslogx/xpluslogx/Program.cs
@@ -9,29 +9,23 @@ namespace xpluslogx
         {
             Console.WriteLine("Podaj dokładność");
             double epsilon = double.Parse(Console.ReadLine());
-            double xZero = FindZeroPoint(0.0, 1.5, epsilon);
+            double xZero = FindZeroPoint(0.0, 1.0, epsilon,0);
             Console.WriteLine($"Wynik: {xZero}");
             Console.ReadKey();
 
         }
-        static double FindZeroPoint(double a,double b, double epsilon)
+        static double FindZeroPoint(double a,double b, double epsilon, int counter)
         {
-            if ((a + ((b - a) / 2)) + Math.Log10(a + ((b - a) / 2)) < epsilon)
-            {
-                Console.WriteLine($"xxx {(a + ((b - a) / 2)) + Math.Log10(a + ((b - a) / 2)}");
-                return (a + ((b - a) / 2));
-            }
-            if (((a + ((b - a) / 2)) + (Math.Log10(a + ((b - a) / 2))) / a+Math.Log10(a)) < 0)
-            {
-                Console.WriteLine("111");
-
-                b = a + ((b - a) / 2);
-                return FindZeroPoint(a, b, epsilon);
-            }
-            
-            a = a + ((b - a) / 2.0);
-            Console.WriteLine("222");
-            return FindZeroPoint(a, b, epsilon);
+            double cpoint = a+((b-a)/2);
+            if ((cpoint + Math.Log10(cpoint)) > 0 && (cpoint + Math.Log10(cpoint)) < epsilon) return cpoint;
+            if (counter % 2 != 0) b = cpoint;
+            else a = cpoint;
+            counter++;
+            return FindZeroPoint(a, b, epsilon, counter);
+        }
+        static double FindLog(double x)
+        {
+            return x + Math.Log10(x);
         }
     }
 }
