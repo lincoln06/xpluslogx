c066423b1b1b17ffa7df2d8498c9b8552d0bdca0
MJL <maciekjl1993@gmail.com>
2022-04-14 18:28:20 +0200

    backup

---
+++ xpluslogx/xpluslogx/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace xpluslogx
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj dokładność");
+            double epsilon = double.Parse(Console.ReadLine());
+            double xZero = FindZeroPoint(0.0, 1.5, epsilon);
+            Console.WriteLine($"Wynik: {xZero}");
+            Console.ReadKey();
+
+        }
+        static double FindZeroPoint(double a,double b, double epsilon)
+        {
+            if ((a + ((b - a) / 2)) + Math.Log10(a + ((b - a) / 2)) < epsilon)
+            {
+                Console.WriteLine($"xxx {(a + ((b - a) / 2)) + Math.Log10(a + ((b - a) / 2)}");
+                return (a + ((b - a) / 2));
+            }
+            if (((a + ((b - a) / 2)) + (Math.Log10(a + ((b - a) / 2))) / a+Math.Log10(a)) < 0)
+            {
+                Console.WriteLine("111");
+
+                b = a + ((b - a) / 2);
+                return FindZeroPoint(a, b, epsilon);
+            }
+            
+            a = a + ((b - a) / 2.0);
+            Console.WriteLine("222");
+            return FindZeroPoint(a, b, epsilon);
+        }
+    }
+}
